34fec551f723475975dce918a59c2978748017ce
Thulio <thulio.alves@gmail.com>
2020-08-04 07:45:41 -0300

    Commit inicial, preparando ambiente, aula 1

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/24438423?s=460&u=78b32cca5c51573c56ba4cb7f54e6a3ee3e9a8e6&v=4"
+          alt="Thulio Alves"
+        />
+        <div>
+          <strong>Thulio Alves</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 50,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
